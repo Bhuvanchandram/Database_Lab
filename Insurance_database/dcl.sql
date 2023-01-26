162a3f947d78aa08442c2656b380c51193049ba6
Bhuvanchandra M <bhuvanchandra668@gmail.com>
2023-01-26 16:57:41 +0530

    Updates

---
+++ Insurance_database/dcl.sql
@@ -0,0 +1,15 @@
+--JOIN operators
+SELECT Person.name,car.Model FROM Person
+INNER JOIN owns ON Person.driver_id=owns.driver_id
+INNER JOIN car ON owns.Reg_no=car.Reg_no;
+
+--WHERE operator
+SELECT Accident.report_number,Accident.location,Person.name FROM Participated
+JOIN Person ON Participated.driver_id=Person.driver_id
+JOIN Car ON Participated.Reg_no=car.Reg_no
+JOIN Accident ON Participated.report_number=Accident.report_number
+WHERE Accident.acc_date BETWEEN "2020-01-01" AND "2021-12-12"
+ORDER BY Participated.damage_amount;
+
+--Using GROUP BY operator
+SELECT car.Model(COUNT)
