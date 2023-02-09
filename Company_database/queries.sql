01d80859575fa941beee8048f65fb4b720a0c55b
Bhuvanchandra M <bhuvanchandra668@gmail.com>
2023-02-09 20:30:18 +0530

    Changes

--- Company_database/queries.sql
+++ Company_database/queries.sql
@@ -5,4 +5,7 @@ Select Pno from works_on where SSN=(Select SSN from Employee where ename LIKE "%
 --In the question it is IoT, in the program it has been changed to ECE
 Update Employee SET salary=salary+(0.10*salary) where Dno=(Select Dno from project where Pname LIKE "%ECE%");
 
---
+--Retrieve the name of each employee who works on all the projects controlled by department number 5 
+Select ename from Employee where SSN=(Select SSN from works_on where Pno=(Select Pno from project where Dno=5) GROUP BY SSN Having COUNT(Pno)=(Select COUNT(*) from project WHERE Dno=5));
+
+--For each department that has more than five employees, retrieve the department number and the number of its employees who are making more than Rs. 6,00,000. 
